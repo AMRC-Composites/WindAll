9a2e6cbaceb88a72167f438d04556d0ccd810149
TimSwait-AMRC <t.swait@amrc.co.uk>
2026-06-15 20:24:09 +0100

    Updated UsefulBoxedTriangleMesh to work with FemMeshs (hopefully...)

--- freecadmacros/utils/trianglemeshutils.py
+++ freecadmacros/utils/trianglemeshutils.py
@@ -1,24 +1,52 @@
-from barmesh.tribarmes import TriangleBarMesh, TriangleBar, MakeTriangleBoxing
-from barmesh.basicgeo import I1, Partition1, P3, P2, Along
-from FreeCAD import Vector
+from barmesh.tribarmes import TriangleBarMesh, MakeTriangleBoxing
+from barmesh.basicgeo import P3
+import Mesh
+import Fem
+
 
 def clamp(num, min_value, max_value):
     return max(min(num, max_value), min_value)
 
+
 class UsefulBoxedTriangleMesh:
-    def __init__(self, meshobject, btriangleboxing=True):
+    """Class to hold a triangle bar mesh object with boxing"""
+
+    def __init__(self, mesh, btriangleboxing=True):
+        """
+        Initialises a UBTM object, requires:
+        mesh: Either  FreeCAD Mesh or 1st order triangular FemMesh
+        btriangleboxing: boolean to determine whether or not to create boxing
+        """
         global tbarmesh, tboxing, hitreg, nhitreg
-        trpts = [ sum(f.Points, ())  for f in meshobject.Facets ]
-        #print("building tbarmesh ", len(trpts))
+        if isinstance(mesh, Mesh.Mesh):
+            trpts = [sum(f.Points, ()) for f in mesh.Facets]
+        elif isinstance(mesh, Fem.FemMesh):
+            assert mesh.TriangleCount > 0, "Mesh must contain triangles"
+            trpts = []
+            for fid in mesh.Faces:
+                fapts = []
+                nids = mesh.getElementNodes(fid)
+                assert len(nids) == 3, "Mesh must be 1st order triangular"
+                nodes = [mesh.getNodeById(nid) for nid in nids]
+                for n in nodes:
+                    fapts.append(n.x)
+                    fapts.append(n.y)
+                    fapts.append(n.z)
+                trpts.append(fapts)
+        print(trpts)
+        # print("building tbarmesh ", len(trpts))
         self.tbarmesh = TriangleBarMesh()
         self.tbarmesh.BuildTriangleBarmesh(trpts)
-        assert len(self.tbarmesh.nodes) == meshobject.CountPoints
-        assert len(self.tbarmesh.bars) == meshobject.CountEdges
+        if isinstance(mesh, Mesh.Mesh):
+            assert len(self.tbarmesh.nodes) == mesh.CountPoints
+            assert len(self.tbarmesh.bars) == mesh.CountEdges
+        elif isinstance(mesh, Fem.FemMesh):
+            assert len(self.tbarmesh.nodes) == mesh.NodeCount
         if btriangleboxing:
             self.tboxing = MakeTriangleBoxing(self.tbarmesh)
             self.hitreg = [0]*len(self.tbarmesh.bars)
             self.nhitreg = 0
-        
+
     def FindClosestEdge(self, p, r=3):
         self.nhitreg += 1
         barclosest = None
@@ -43,7 +71,8 @@ class UsefulBoxedTriangleMesh:
                         barclosestlam = lam
                     self.hitreg[i] = self.nhitreg
         return barclosest, barclosestlam
-        
+
+
 def facetbetweenbars(bar, barN):
     assert bar != barN
     nodeR = bar.barforeright.GetNodeFore(bar.barforeright.nodeback == bar.nodefore)
@@ -63,14 +92,16 @@ def facetbetweenbars(bar, barN):
             tbar = bar.barbackleft.GetForeRightBL(bar.barbackleft.nodeback == bar.nodeback)
         else:
             tbar = bar.barbackleft
-            
+
     DtbarR = tbar.barforeright.GetNodeFore(tbar.barforeright.nodeback == tbar.nodefore)
     assert DtbarR.i > tbar.nodeback.i
-    Dtbars = [ tbar, tbar.barforeright, 
-               tbar.barforeright.GetForeRightBL(tbar.barforeright.nodeback == tbar.nodefore) ]
+    Dtbars = [tbar, tbar.barforeright,
+              tbar.barforeright.GetForeRightBL(tbar.barforeright.nodeback == 
+                                               tbar.nodefore)]
     assert bar in Dtbars and barN in Dtbars
     return tbar
-    
+
+
 def GetBarForeLeftBRN(tbar, bforeleft):
     tbarR = tbar
     Dcounter = 0
@@ -78,7 +109,7 @@ def GetBarForeLeftBRN(tbar, bforeleft):
     while True:
         assert tbarR.nodeback == nodeC or tbarR.nodefore == nodeC
         tbarRnext = tbarR.GetForeRightBL(tbarR.nodefore == nodeC)
-        if tbarRnext == None or tbarRnext == tbar:
+        if tbarRnext is None or tbarRnext == tbar:
             break
         tbarR = tbarRnext
         Dcounter += 1
